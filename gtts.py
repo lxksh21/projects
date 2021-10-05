bd03bcb8249c65e7b5342292464550a97582c380
Abhishek <70463156+abh1shekgupta@users.noreply.github.com>
2021-10-05 23:06:47 +0530

    Create gtts.py

---
+++ gtts.py
@@ -0,0 +1,8 @@
+import gtts
+from gtts import gTTS
+import os
+
+my_message='Hello this is text to speech sample'
+my_val=gtts.gTTS(text=my_message,slow=False,lang='en')
+my_val.save('testmessage3.mp3')
+os.system('testmessage3.mp3')
